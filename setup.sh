5806d83796f44468b6e07528653876d2eec3071a
heilgar <2695198+heilgar@users.noreply.github.com>
2022-09-30 17:39:13 +0000

    update setup

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
+echo "Installing required packages"
 pip install -r requirements.txt
+echo "Copying service"
 cp ./alarm.service /etc/systemd/system/alarm.service
+echo "Staring service"
 systemctl enable alarm.service
 systemctl daemon-reload
 systemctl start alarm.service
+echo "Generating serial"
+python serial.py
